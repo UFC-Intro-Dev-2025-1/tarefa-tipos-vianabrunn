d3982265d1f457bf13cd154559937dd621b629f9
Bruna Viana <164355357+vianabrunn@users.noreply.github.com>
2025-05-22 20:15:23 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-// Escreva seu código aqui!
+let mesas : number = 30
+console.log(mesas);
+
+let usuario : string = 'bruna'
+console.log(usuario);
+
+let temAula : boolean = true
+console.log(temAula);
+
+let brincadeira: undefined;
+console.log(brincadeira);
+
+interface objeto {
+    marca: string;
+    quatidade: number;
+    eficiencia: boolean;
+    idade: undefined;
+}
+
+let mouse: objeto = {
+    marca: 'dell',
+    quatidade: 30,
+    eficiencia: true,
+    idade: undefined
+};
+
+console.log(mouse);
